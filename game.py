70a96de62851248d4131e3d72fe0f4b15a69f6fe
Bryce Eakin <bryce@mighty.com>
2020-02-08 12:51:30 -0500

    Made it a bit cleaner and added comments

--- game.py
+++ game.py
@@ -14,11 +14,11 @@ import pyxel
 # - [optional] what color to make transparent (-1 means no transparency)
 
 # Background Sprites
-SKY_SPRITE = (0, 0, 88, 160, 32, -1)
-MOUNTAIN_SPRITE = (0, 0, 64, 160, 24, 12)
-FOREST_SPRITE = (0, 0, 48, 160, 16, 12)
-FAR_CLOUD_SPRITE = (0, 64, 32, 32, 8, 12)
-NEAR_CLOUD_SPRITE = (0, 0, 32, 56, 8, 12)
+SKY_SPRITE =        (0,  0, 88, 160, 32, -1)
+MOUNTAIN_SPRITE =   (0,  0, 64, 160, 24, 12)
+FOREST_SPRITE =     (0,  0, 48, 160, 16, 12)
+FAR_CLOUD_SPRITE =  (0, 64, 32, 32,   8, 12)
+NEAR_CLOUD_SPRITE = (0,  0, 32, 56,   8, 12)
 
 # Foreground Sprites
 FLOOR_SPRITE = (0, 0, 16, 40, 8, 12)
@@ -28,38 +28,50 @@ FRUIT_SPRITE = [
     (0, 64, 0, 16, 16, 12)
 ]
 PLAYER_SPRITE = [
-    (0, ),
-    (0, )
+    (0,  0, 0, 16, 16, 12,),  # Player when falling
+    (0, 16, 0, 16, 16, 12,),  # Player when jumping
 ]
 
 BASE_FPS = 30
+
 
 class App:
     def __init__(self, fps=BASE_FPS, speed=1.0):
         pyxel.init(160, 120, caption="Pyxel Jump", fps=fps)
 
+        # Load all the images and sounds used in the game
         pyxel.load("assets/jump_game.pyxres")
 
-        self._frame_step = speed * BASE_FPS / fps
         self.__speed = speed
         self._fps = fps
 
-        self.frame_count = 0
+        # Factor to keep things running normally at different fps / speeds
+        self._frame_step = speed * BASE_FPS / fps
+
+        # number of frames that would have happened so far at current speed/fps
+        # (adjusted when speed changes to keep things rendering at the same spot)
+        self.frame_count = 0  
+
+        # Seconds of 1x-speed game time since game start
         self.game_time = 0.0
 
+        # Player info
         self.score = 0
         self.player_x = 72
         self.player_y = -16
         self.player_vy = 0
         self.player_is_alive = True
 
+        # Define the stuff on screen
         self.far_cloud = [(-10, 75), (40, 65), (90, 60)]
         self.near_cloud = [(10, 25), (70, 35), (120, 15)]
         self.floor = [(i * 60, randint(32, 104), True) for i in range(4)]
         self.fruit = [(i * 60, randint(40, 104), randint(0, 2), True) for i in range(4)]
 
+        # Start playing music (sound #0)
         pyxel.playm(0, loop=True)
 
+        # Run the game (update/draw loop)
         pyxel.run(self.update, self.draw)
 
     @property
@@ -68,6 +80,9 @@ class App:
 
     @_speed.setter
     def _speed(self, newval):
+        # When speed changes we have to update the frame counter or background things 
+        # will suddenly jump around
+        
         ratio = newval/self.__speed
         self.frame_count /= ratio
         self._frame_step *= ratio
@@ -194,16 +209,7 @@ class App:
                 pyxel.blt(x, y, *FRUIT_SPRITE[kind])
 
         # draw player
-        pyxel.blt(
-            self.player_x,
-            self.player_y,
-            0,
-            16 if self.player_vy > 0 else 0,
-            0,
-            16,
-            16,
-            12,
-        )
+        pyxel.blt(self.player_x, self.player_y, *PLAYER_SPRITE[1 if self.player_vy > 0 else 0])
 
         # draw score
         s = "SCORE {:>4}".format(self.score)
